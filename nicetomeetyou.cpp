0057082ba59139bd9f35be21956d32a3d0eac00c
nektarioskouro <54715648+nektarioskouro@users.noreply.github.com>
2020-02-06 05:18:49 -0500

    Add files via upload

---
+++ nicetomeetyou.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   string x;
+   cout << "What's your name?" << endl;
+   cin >> x;
+   cout << "Nice to meet you " << x;
+   return 0;
+}
